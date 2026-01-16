7d33d0d932d68ccc253e563c643f8d3f935e6250
Breland Miley <breland@bdawg.org>
2026-01-16 14:06:31 -0800

    Flyway disable

--- src/test/java/io/breland/bbagent/server/BBChatGptAgentApplicationTests.java
+++ src/test/java/io/breland/bbagent/server/BBChatGptAgentApplicationTests.java
@@ -3,7 +3,7 @@ package io.breland.bbagent.server;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.flyway.enabled=false"})
 class BBChatGptAgentApplicationTests {
 
   @Test
