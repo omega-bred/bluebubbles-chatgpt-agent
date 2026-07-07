b5a7f7b535a3f7eaa23f725418547857cc8ff1c5
Breland Miley <breland@bdawg.org>
2026-07-07 09:10:03 -0700

    Remove raw incoming message logs

--- src/main/java/io/breland/bbagent/server/controllers/BluebubblesWebhookController.java
+++ src/main/java/io/breland/bbagent/server/controllers/BluebubblesWebhookController.java
@@ -47,7 +47,6 @@ public class BluebubblesWebhookController extends BluebubblesApiController {
   @Override
   public ResponseEntity<Object> bluebubblesMessageReceived(
       @Valid @RequestBody BlueBubblesMessageReceivedRequest requestBody) {
-    log.info("Incoming Message {}", requestBody);
     if (requestBody == null || requestBody.getType() == null) {
       return ResponseEntity.badRequest().build();
     }
