acfccb87e182a04370b41c44a14b796b06f723ca
Breland Miley <breland@bdawg.org>
2026-08-06 18:19:28 +0200

    Use Lombok for workflow properties (#227)

--- src/main/java/io/breland/bbagent/server/agent/AgentWorkflowProperties.java
+++ src/main/java/io/breland/bbagent/server/agent/AgentWorkflowProperties.java
@@ -1,46 +1,18 @@
 package io.breland.bbagent.server.agent;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "agent.workflow")
+@Getter
+@Setter
 public class AgentWorkflowProperties {
 
   private String cadenceDomain = "default";
   private String cadenceTaskList = "bbagent";
   private String cadenceHost = "localhost";
   private int cadencePort = 7933;
-
-  public String getCadenceDomain() {
-    return cadenceDomain;
-  }
-
-  public void setCadenceDomain(String cadenceDomain) {
-    this.cadenceDomain = cadenceDomain;
-  }
-
-  public String getCadenceTaskList() {
-    return cadenceTaskList;
-  }
-
-  public void setCadenceTaskList(String cadenceTaskList) {
-    this.cadenceTaskList = cadenceTaskList;
-  }
-
-  public String getCadenceHost() {
-    return cadenceHost;
-  }
-
-  public void setCadenceHost(String cadenceHost) {
-    this.cadenceHost = cadenceHost;
-  }
-
-  public int getCadencePort() {
-    return cadencePort;
-  }
-
-  public void setCadencePort(int cadencePort) {
-    this.cadencePort = cadencePort;
-  }
 }
